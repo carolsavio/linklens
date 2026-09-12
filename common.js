9fc9417f531adb91a35fd8753be1717ecc3dba26
carolsavio <aploriart@gmail.com>
2026-09-12 18:46:05 -0300

    adding hyphenation heuristic

--- common.js
+++ common.js
@@ -2,10 +2,23 @@ function analisarUrl(url) {
     let score = 0;
     const motivos = [];
     const host = new URL(url).hostname;
+
+    // Heurística 1 -> O domínio é um IP em vez de um nome? ReGex
+    if (/^\d{1,3}(\.\d{1,3}){3}$/.test(host)) {
+        score += 30;
+        motivos.push("Domínio é um endereço IP");
+    }
+
+    // o dominio tem muitos hífens?
+    if ((host.match(/-/g) || [].length > 2)){
+        score += 15;
+        motivos.push("Domínio com muitos hífens");
+    }
+
+    return {
+        score,
+        motivos
+    };
+
 }
 
-// Heurística 1 -> O domínio é um IP em vez de um nome? ReGex
-if ("regex do ipv4") {
-    score += 30;
-    motivos.push("Domínio é um endereço IP");
-}
